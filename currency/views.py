d798d093cde89158d682fab84cd0e16044905155
ItsMeSwaron <swaron3214@gmail.com>
2025-06-28 00:39:17 +0600

    requirements added

--- currency/views.py
+++ currency/views.py
@@ -1,17 +1,37 @@
-import requests
-from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from django.conf import settings
+import requests
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class CurrencyRateAPIView(APIView):
     permission_classes = [AllowAny]
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'base_currency',
+                openapi.IN_QUERY,
+                description="The base currency (e.g., USD)",
+                type=openapi.TYPE_STRING,
+                required=False
+            ),
+            openapi.Parameter(
+                'currencies',
+                openapi.IN_QUERY,
+                description="Comma-separated currency codes like BDT,EUR",
+                type=openapi.TYPE_STRING,
+                required=False
+            ),
+        ]
+    )
     def get(self, request):
         base = request.query_params.get('base_currency', 'USD')
         currencies = request.query_params.get('currencies', 'BDT,EUR')
 
-        url = f"https://api.freecurrencyapi.com/v1/latest"
+        url = "https://api.freecurrencyapi.com/v1/latest"
         params = {
             "apikey": settings.FREE_CURRENCY_API_KEY,
             "base_currency": base,
