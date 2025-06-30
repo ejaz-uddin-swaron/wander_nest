af61b59163474dfb3a87ec18779a437c708ba999
ItsMeSwaron <swaron3214@gmail.com>
2025-06-30 10:56:20 +0600

    restaurants admin added

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Restaurant
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'price', 'rating')
+    search_fields = ('name', 'location', 'cuisine')
+    list_filter = ('tags',)
