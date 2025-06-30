1dddd21b9acddcc7911587da70e17596dc427719
ItsMeSwaron <swaron3214@gmail.com>
2025-06-30 11:55:24 +0600

    imagefield

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    image = models.CharField(max_length=255)  # For simplicity; use ImageField if handling uploads
+    image = models.ImageField(upload_to='restaurants/')
     rating = models.CharField(max_length=50)
     cuisine = models.CharField(max_length=100)
     price = models.IntegerField()
