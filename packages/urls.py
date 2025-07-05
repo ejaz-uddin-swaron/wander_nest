053ad8439cce53f1a1ac88113a304694f44d1dc6
ItsMeSwaron <swaron3214@gmail.com>
2025-07-05 20:56:17 +0600

    FINAL 3

--- packages/urls.py
+++ packages/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('api/packages/transport-options/', TransportOptionListView.as_view()),
-    path('api/packages/hotel-options/', HotelOptionListView.as_view()),
-    path('api/packages/guide-options/', GuideOptionListView.as_view()),
-    path('api/packages/create/', PackageCreateView.as_view()),
+    path('transport-options/', TransportOptionListView.as_view()),
+    path('hotel-options/', HotelOptionListView.as_view()),
+    path('guide-options/', GuideOptionListView.as_view()),
+    path('create/', PackageCreateView.as_view()),
     path('all/', PreMadePackageListView.as_view(), name='premade-packages'),
 ]
